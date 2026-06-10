44d2acd9394c89e2bde9ede37cd145078900599c
Alfredo-capitia <delciiocapitia676@gmail.com>
2026-06-11 00:08:34 +0100

    fix : fix the syntax and path directory

--- nginx_log_analizer.sh
+++ nginx_log_analizer.sh
@@ -1,6 +1,5 @@
-s#!/bin/bash
-
-set -euo pipefail
+#!/bin/bash
+set -uo pipefail
 
 FILE_LOG="/home/levelup007/devops-course/linux/nginx_log_analizer/nginx-access.log"
 
@@ -11,11 +10,11 @@ read_log() {
   echo " =============== Top 5 paths with most requests  ==============="
   awk '{print $7}' $FILE_LOG | sort | uniq -c | sort -rn | head -5
 
- echo " ========== Top 5 status code with most requests ================ "
- awk '{print $9}' $FILE_LOG | sort | uniq -c | sort -rn | head -5
+  echo " ========== Top 5 status code with most requests ================ "
+  awk '{print $9}' $FILE_LOG | sort | uniq -c | sort -rn | head -5
 
- echo " ============ Top 5 user agents ============" 
-  awk '{print $12}' $LOG_FILE | sort | uniq -c | sort -rn | head -5
+  echo " ============ Top 5 user agents ============" 
+  awk '{print $12}' $FILE_LOG | sort | uniq -c | sort -rn | head -5
 }
 
 read_log
